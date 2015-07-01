6f2bdaf6e6ff94d00d0c620b96d3404a7af8a87f
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2015-07-01 22:17:40 +0900

    rename in Main func.
    これは情報理論とは関係ないと思われるのでAliceとかBobの出番はもはやない．

--- src/game/Main.java
+++ src/game/Main.java
@@ -7,15 +7,15 @@ import enums.Species;
 public class Main {
 	public static void main(String[] args) {
 
-		Guild alice = new Guild("alice");
-		alice.acceptHunter(new Hunter("Bob", 1, 100, 10, 20));
-		alice.acceptHunter(new Hunter("Carroll", 10, 1000, 100, 200));
-		alice.makeQuests("Donut", new Item(ItemName.Iron), new Monster("ウルフ", 3, 50, 10, 10, Species.Wolf, new Item(ItemName.Leather)), 3);
-		alice.makeQuests("CupCake", new Item(ItemName.Iron), new Monster("ゴブリン", 1, 30, 5, 5, Species.Goblin, new Item(ItemName.Bone)), 1);
-		alice.makeQuests("Eclair", new Item(ItemName.Iron), new BigMonster("ドラゴン", 5, 100, 15, 15, Species.Dragon, new Item(ItemName.Tail), 30), 1);
+		Guild garm = new Guild("Garm");
+		garm.acceptHunter(new Hunter("Alex", 1, 100, 10, 20));
+		garm.acceptHunter(new Hunter("Betty", 10, 1000, 100, 200));
+		garm.makeQuests("Novice01", new Item(ItemName.Iron), new Monster("ウルフ", 3, 50, 10, 10, Species.Wolf, new Item(ItemName.Leather)), 3);
+		garm.makeQuests("Novice02", new Item(ItemName.Iron), new Monster("ゴブリン", 1, 30, 5, 5, Species.Goblin, new Item(ItemName.Bone)), 1);
+		garm.makeQuests("Expert01", new Item(ItemName.Iron), new BigMonster("ドラゴン", 5, 100, 15, 15, Species.Dragon, new Item(ItemName.Tail), 30), 1);
 
-		alice.hunters.get(0).quest = alice.getQuest();
-		alice.hunters.get(0).battle(alice.hunters.get(0).quest.monsters.poll());
+		garm.hunters.get(0).quest = alice.getQuest();
+		garm.hunters.get(0).battle(alice.hunters.get(0).quest.monsters.poll());
 
 	}
 }
