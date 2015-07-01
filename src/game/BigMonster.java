4336f6db646ea4ae304205b2d8c563ab572a296c
Ryoto Tomioka <tommy.ryoto93@gmail.com>
2015-07-01 23:55:28 +0900

    BigMonster by Shoko
    special attack!!

--- src/game/BigMonster.java
+++ src/game/BigMonster.java
@@ -1,19 +1,36 @@
 package game;
 
+import java.util.Random;
+
 import enums.Species;
 
 public class BigMonster extends Monster{
 
 	protected int specialAP;
+	int ran = (int)(Math.random()*2);
 
 	public BigMonster(String _name, int _level, int _maxHP, int _AP, int _DP, Species _species, Item _dropItem, int _specialAP) {
 		super(_name, _level, _maxHP, _AP, _DP, _species, _dropItem);
 		specialAP = _specialAP;
 	}
 
-	public void specialAttack() {
+	public void attack(Character _char) {
+		if(ran==1){
+			int tmp = specialAP - (rnd.nextInt(20)-10) + DP;
+			_char.HP = _char.HP - tmp <0 ? 0 : _char.HP - tmp ;
+			System.out.println(name + "は" + _char.name + "に" + tmp + "特別ダメージを与えた！");
+			System.out.println(_char.name + "の残りHP : " + _char.HP);
+			if(_char.HP==0)
+				System.out.println(_char.name + "は死んだ。");
+		}
+		else{
+			int tmp = AP - (rnd.nextInt(20)-10) + DP;
+			_char.HP = _char.HP - tmp <0 ? 0 : _char.HP - tmp ;
+			System.out.println(name + "は" + _char.name + "に" + tmp + "ダメージを与えた！");
+			System.out.println(_char.name + "の残りHP : " + _char.HP);
+			if(_char.HP==0)
+				System.out.println(_char.name + "は死んだ。");
+		}
 
 	}
-
-
 }
